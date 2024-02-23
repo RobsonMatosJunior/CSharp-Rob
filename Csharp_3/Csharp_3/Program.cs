2901e46848d68d3037be36f252c0cdf3423cb4e9
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-23 20:46:40 -0300

    fim da atividade

--- Csharp_3/Csharp_3/Program.cs
+++ Csharp_3/Csharp_3/Program.cs
@@ -15,12 +15,21 @@ namespace Csharp_3
             Console.WriteLine("Entre com o preço de um produto!");
             string precoInput = Console.ReadLine().Replace(',', '.'); // Substituir vírgula por ponto
             double preco = double.Parse(precoInput, CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)!");
+            string[] vet = Console.ReadLine().Split(' ');
+            string sobrenome = vet[0];
+            int idade = int.Parse(vet[1]);
+            string alturaInput = vet[2].Replace(',', '.');
+            double altura = double.Parse(alturaInput, CultureInfo.InvariantCulture);
 
 
             //Answer + content
             Console.WriteLine("Seu nome é: " + nome);
             Console.WriteLine("Em sua casa tem: " + quartos + " quartos!");
-           Console.WriteLine("O preço do produto é: " + preco.ToString("F2", CultureInfo.GetCultureInfo("pt-BR")));
+            Console.WriteLine("O preço do produto é: " + preco.ToString("F2", CultureInfo.GetCultureInfo("pt-BR"))); 
+            Console.WriteLine("Seu Sobrenome é: " + sobrenome);
+            Console.WriteLine("Sua idade é: " + idade);
+            Console.WriteLine("Você mede: " + altura.ToString("F2", CultureInfo.GetCultureInfo("pt-BR")));
 
         }
     }
