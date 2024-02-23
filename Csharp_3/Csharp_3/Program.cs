b5cac8534777d7ba402ee2912fea4be0dcac6e5e
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-23 20:21:19 -0300

    exercicio 1  C#

---
+++ Csharp_3/Csharp_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Csharp_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Questionnaire
+            Console.WriteLine("Entre com seu nome!");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos Quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto!");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            //Answer + content
+            Console.WriteLine("Seu nome é: " + nome);
+            Console.WriteLine("Em sua casa tem: " + quartos + " quartos!");
+            Console.WriteLine("O preço do produto é: " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
+
