cf7b00ea94c52f20eded8ba61af60ff4e655dae3
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 15:25:32 -0300

    finish Problema 1017

---
+++ Problema/Problema1016/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+            int tempo = distancia * 2;
+            Console.WriteLine(tempo + " minutos");
+
+            Console.ReadLine();
+        }
+    }
