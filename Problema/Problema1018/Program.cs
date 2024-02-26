1a5a6c31de9b97a11561e6d4aacaf3b2e55d6227
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 16:14:39 -0300

    finish Problema 1018

---
+++ Problema/Problema1018/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            int[] notas = {100, 50, 20, 10, 5, 2, 1 };
+ 
+            Console.WriteLine(valor);
+
+
+            foreach (int nota in notas)
+            {
+                int quantidadeNotas = valor / nota;
+                Console.WriteLine(quantidadeNotas + " nota(s) de R$ " + nota + ",00");
+                valor %= nota;
+                
+            }
+        }
+    }
