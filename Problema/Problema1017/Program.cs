0e4a8ed12317c4e24c277b6ccfee05998a21e832
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 15:40:37 -0300

    finish problema 1017

---
+++ Problema/Problema1017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double distancia = tempo * velocidade;
+            double litros = distancia / 12;
+
+            Console.WriteLine(litros.ToString("F3"));
+            Console.ReadLine();
+        }
+    }
