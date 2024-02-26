349a94dc6f0eed113521e8ba3b3de6969dd39a84
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 10:23:12 -0300

    testes URI

---
+++ Csharp_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Csharp_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Raio, Area, Pi;
+            Raio = double.Parse(Console.ReadLine());
+            Pi = 3.14159;
+            Area = Pi * Math.Pow(Raio, 2);
+            Console.WriteLine("A= " + Area.ToString("F4"));
+            Console.ReadLine();
+
+        }
+    }
+}
