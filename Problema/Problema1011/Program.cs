507890fa16bcf9de7201fb1ab32cd461dc940137
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 12:54:58 -0300

    tst

---
+++ Problema/Problema1011/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double Raio, Pi, Volume;
+        Raio = double.Parse(Console.ReadLine());
+        Pi = 3.14159;
+        Volume = (4.0 / 3) * Pi * Math.Pow(Raio, 3);
+        Console.WriteLine("VOLUME = " + Volume.ToString("F3"));
+        Console.ReadLine();
+    }
+}
+
+
