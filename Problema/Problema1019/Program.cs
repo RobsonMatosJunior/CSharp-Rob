6431ed3a8246f0fa2834f8e8ae70679f3e5d7027
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-26 21:37:16 -0300

    finish problema 1019

---
+++ Problema/Problema1019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos = int.Parse(Console.ReadLine());
+            int horas = segundos / 3600;
+            segundos %= 3600;
+            int minutos = segundos / 60;
+            segundos %= 60;
+
+            Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+        }
+    }
