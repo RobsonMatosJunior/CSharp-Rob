d9fde49a3926259243f604ee2c36bb435c04541c
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-27 09:43:34 -0300

    ajuste de hierarquia

---
+++ Aulas/Csharp_10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Csharp_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Valor1= Console.ReadLine().Split(' ');
+            decimal a = decimal.Parse(Valor1[0]);
+            decimal b = decimal.Parse(Valor1[1]);
+            decimal c = decimal.Parse(Valor1[2]);
+
+            string[] Valor2= Console.ReadLine().Split(' ');
+            decimal d = decimal.Parse(Valor2[0]);
+            decimal e = decimal.Parse(Valor2[1]);
+            decimal f = decimal.Parse(Valor2[2]);
+
+            decimal resultado = (b * c) + (e * f);
+            Console.WriteLine("VALOR A PAGAR: R$ " + resultado.ToString("F2"));
+        }
+
+    }
+}
---
+++ Aulas/Csharp_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string nomeVendedor = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine());
+            double totalVendas = double.Parse(Console.ReadLine());
+
+            double comissao = totalVendas * 0.15;
+            double totalReceber = salarioFixo + comissao;
+
+            Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2"));
+            Console.ReadLine();
+
+        }
+    }
+}
