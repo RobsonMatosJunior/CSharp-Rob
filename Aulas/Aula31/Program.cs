9ac79f5092ecc9551a25fe54dfb312a9e5631d16
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-02-28 17:31:09 -0300

    Finish Aula 31

---
+++ Aulas/Aula31/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Operadores Lógicos
+        && = // E
+        || = // OU
+        ! = // NÃO
+
+        // Exemplo de && (E):
+
+        Para obter uma habilitação é necessário:
+        Ter 18 anos ou mais
+        &&
+        Ser aprovado no psicotécnico
+        &&
+        Aprovado no exame de Legislação
+        &&
+        Ser aprovado no exame de direção.
+        // Todas as condições devem ser verdadeiras!
+        
+        // Exemplo de || (OU):
+
+
+
+        
+    }
+}
