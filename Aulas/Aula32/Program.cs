6f1d1d4d20c9faa154b1d78bf6882e4c7463c049
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-03-02 15:19:56 -0300

    meio da aula

--- Aulas/Aula32/Program.cs
+++ Aulas/Aula32/Program.cs
@@ -1,8 +1,26 @@
 ﻿
+// Estrutura condicional
 
+// Sintaxe da estrutura condicional:
+
+// True = executa o bloco de comandos
+// False = pula o bloco de comandos
+// Exemplo simples:
+/* Simples:
+    if (<condição>){
+        <comando 1>
+        <comando 2>
+    } */
 
 using System;
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+
+            Console.ReadLine();
+        }
     }
