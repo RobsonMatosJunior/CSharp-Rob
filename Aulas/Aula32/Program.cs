ebcd8468ac888f07d8f3db551fe9f6cb69ad11ad
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-03-02 17:47:28 -0300

    fim aula 32

--- Aulas/Aula32/Program.cs
+++ Aulas/Aula32/Program.cs
@@ -12,15 +12,55 @@
         <comando 2>
     } */
 
-using System;
+/* using System;
     class Program
     {
         static void Main(string[] args)
         {
+            int x = 5;
             Console.WriteLine("Bom dia!");
-            Console.WriteLine("Boa tarde");
+            if (x<0) 
+            // Se o retorno for verdadeiro será impresso a mensagem.
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            else
+            {
+
+            }
+
             Console.WriteLine("Boa noite");
 
             Console.ReadLine();
         }
+    } */
+
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+            Console.WriteLine("Quantas horas?");
+            horas = int.Parse(Console.ReadLine());
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (horas <= 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+            Console.ReadLine();
+        }
     }
+
+/*
+    Estruturas condicionais Simples
+    Estruturas condicionais compostas
+    Encadeamento de estruturas condicionais
+*/
