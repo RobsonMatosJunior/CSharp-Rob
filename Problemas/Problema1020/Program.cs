75b169e3c8eb5b29ac8e0248bb70768e55a656ba
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-03-03 19:10:16 -0300

    Ajuste de nomeclatura de pastas, fim aula 34, fim problema 1020 e aula 34 = problema 1036

---
+++ Problemas/Problema1020/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+using System;
+class program
+{
+    static void Main(string [] args)
+    {
+        int dias = int.Parse(Console.ReadLine());
+        
+        int anos = dias / 365;
+        dias = dias % 365;
+        
+        int meses = dias / 30;
+        dias = dias % 30;
+
+        Console.WriteLine(anos + " ano(s)");
+        Console.WriteLine(meses + " mes(es)");
+        Console.WriteLine(dias + " dia(s)");
+    }
+}
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dias = int.Parse(Console.ReadLine());
+
+        int anos = dias / 365;
+        dias = dias % 365;
+
+        int meses = dias / 30;
+        dias = dias % 30;
+
+        Console.WriteLine(anos + " ano(s)");
+        Console.WriteLine(meses + " mes(es)");
+        Console.WriteLine(dias + " dia(s)");
+    }
+}
