f8135d29dc57f10480f3e2da37619584bc03382e
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-03-20 21:57:56 -0300

    finish Aula36

--- Aulas/Aula36/Program.cs
+++ Aulas/Aula36/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int minutos;
+        double conta;
+
+        minutos = int.Parse(Console.ReadLine());
+        conta = 50.0;
+        if (minutos > 100)
+        {
+            conta = conta + (minutos - 100) * 2.0;
+            
+        }
+        Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2"));
+        Console.ReadLine();
+    }
+}
