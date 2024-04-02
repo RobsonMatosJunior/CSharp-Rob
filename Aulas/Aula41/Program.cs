12ea9d4bc3fa1b6ebc3d1bd93e1bd40a00745006
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-04-02 08:17:02 -0300

    Teste de mesa (While) Aula 41

---
+++ Aulas/Aula41/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string [] strings)
+    {
+        int x, y;
+        x = 0;
+        y = 4;
+
+        while(x < 3)
+        {
+            y = y + 2;
+            x = x + 1;
+            Console.WriteLine("x = " + x + " y = " + y);
+        }
+    }
+}
