a13623b612b8e8796666cd3bd602ad19c4ac8d97
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-04-03 10:14:25 -0300

    Finish Aula 43

--- Aulas/Aula43/Program.cs
+++ Aulas/Aula43/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string [] args)
+    {
+        int x, y;
+        
+        string[] vet = Console.ReadLine().Split(' ');
+        x = int.Parse(vet[0]);
+        y = int.Parse(vet[1]);
+
+        while ( x != y)
+        {
+            if (x < y)
+            {
+                Console.WriteLine("Crescente");
+            }
+            else
+            {
+                Console.WriteLine("Decrescente");
+            }
+            vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+        }
+    }
+}
