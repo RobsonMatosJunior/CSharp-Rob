530ba019d2d555e46f24c3077468734b4b92720a
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-04-22 11:17:25 -0300

    remoção de arquivos

--- Aulas/Aula46/obj/Debug/net8.0/Aula46.AssemblyInfo.cs
+++ Aulas/Aula46/obj/Debug/net8.0/Aula46.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d8ce8653b52b441d456be177c6350851564f3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df6aab25375d75e20157e4f0ed738d7b7f6b5e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
